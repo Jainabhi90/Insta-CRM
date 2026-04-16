434a949cf153317737daf0dc61d6f39f51219a34
Abhi Jain <jainabhi90980@gmail.com>
2026-04-16 18:16:16 +0530

    fix: route auth session endpoint to real backend app

--- api/auth/session.js
+++ api/auth/session.js
@@ -1,9 +1,1 @@
-export default function handler(req, res) {
-  // Return empty session
-  if (req.method === 'GET') {
-    return res.status(200).json({});
-  }
-
-  // Logout/others
-  return res.status(200).json({ message: "Success" });
-}
+module.exports = require("../../backend/app")
